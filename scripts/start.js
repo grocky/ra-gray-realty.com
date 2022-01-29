77ad101233a0f433b1ae19cfa095e1da5dca626b
Rocky Gray <rocky.grayjr@gmail.com>
2022-01-29 10:20:19 -0500

    initialize landing page

---
+++ scripts/start.js
@@ -0,0 +1,17 @@
+const upath = require('upath');
+const { exec } = require('child_process');
+
+const browserSyncPath = upath.resolve(upath.dirname(__filename), '../node_modules/.bin/browser-sync');
+
+
+exec(`"${browserSyncPath}" --reload-delay 2000 --reload-debounce 2000 ./ -w --no-online`, (error, stdout, stderr) => {
+  if (error) {
+    console.log(`error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.log(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(stdout);
+});
